1460464ef883d0e24c8b4c598a2ed1bba59df05b
Alvaro Graves <alvaro@graves.cl>
2012-11-17 05:19:13 -0800

    Real first import

--- lodspeakr/settings.inc.php
+++ lodspeakr/settings.inc.php
@@ -1,1 +1,19 @@
-../repo/settings.inc.php
+<?php
+
+$conf['endpoint']['local'] = 'http://graves.cl/bioequivalencia/db/sparql.php';
+$conf['home'] = '/home/alangrafu/graves.cl/bioequivalencia/lodspeakr/';
+$conf['basedir'] = 'http://graves.cl/bioequivalencia/';
+$conf['debug'] = false;
+
+/*ATTENTION: By default this application is available to
+ * be exported and copied (its configuration)
+ * by others. If you do not want that, 
+ * turn the next option as false
+ */ 
+
+# If you want to add/override a namespace, add it here
+$conf['ns']['local']   = 'http://graves.cl/bioequivalencia/';
+$conf['root'] = 'inicio';
+
+$conf['mirror_external_uris'] = false;
+?>
